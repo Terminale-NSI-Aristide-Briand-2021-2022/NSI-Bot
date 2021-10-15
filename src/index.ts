1a765b23d2a22b522ca5f02e1258529a4b3baf05
fantomitechno <fantomitechno@gmail.com>
2021-10-15 21:40:02 +0200

    feat: add Bini as dev

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,11 @@ require('dotenv').config();
 
 export const client = new Bot(
 	{
-		devs: ['563749920683720709', '534822147789553705'],
+		devs: [
+			'563749920683720709', // fantomitechno
+			'534822147789553705', // Kevinlny
+			'531105808243490870'  // Bini
+		],
 		inDev: true,
 	},
 	{
